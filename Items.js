98c941d2f3d6be8f493315de6c60acaafcac8c65
Emil <eeremiev@chequemotiva.com>
2022-06-23 19:11:40 +0200

    first commit

---
+++ Items.js
@@ -0,0 +1,16 @@
+let items = [
+  {
+    id: '1',
+    name: 'test 1',
+  },
+  {
+    id: '2',
+    name: 'test 2',
+  },
+  {
+    id: '3',
+    name: 'test 2',
+  },
+];
+
+module.exports = items;
